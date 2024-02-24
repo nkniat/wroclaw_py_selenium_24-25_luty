f092691ddbcb720970991c7c54759789209afad3
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-02-24 13:52:28 +0100

    selenium, dodatkowe komendy

--- cw4_poruszanie_sie.py
+++ cw4_poruszanie_sie.py
@@ -60,5 +60,60 @@ driver.switch_to.frame(driver.find_element("id", "iframeResult"))
 name = driver.find_element("id", "fname")
 name.send_keys("Natalia")
 
+# wprowadź nazwisko
+name = driver.find_element("id", "lname")
+if name.is_enabled():
+    name.send_keys("Burda")
+else:
+    print("Nie da się wpisać nazwiska")
+
+# zamikniecie zakładki
+driver.close()
+
+# przełącznie sie do bieżącego okna
+driver.switch_to.window(currentWindows)
+print("Aktualne okno to: ", driver.title)
+
+# cofniecie się
+driver.back()
+
+# wybranie menu checkboxa
+checkbox = driver.find_element("xpath", "//*[@id='main']/table[2]/tbody/tr[6]/td[1]/a")
+checkbox.click()
+
+tryIt = driver.find_element("xpath", "//*[@id='main']/div[2]/a")
+tryIt.click()
+
+print("Aktualne okno:", driver.title)
+
+# obecna zakładka
+currentWindows = driver.current_window_handle
+
+# wszystkio okna
+windowsNames = driver.window_handles
+
+for window in windowsNames:
+    if window != currentWindows:
+        driver.switch_to.window(window)
+
+print("Aktualne okno - po przełączeniu:", driver.title)
+
+# przełączenie się do iframe - czyli strony w środku strony
+driver.switch_to.frame(driver.find_element("id", "iframeResult"))
+
+# zaznacz, ze masz samochod (car)
+car = driver.find_element("name", "vehicle2")
+car.click()
+
+if car.is_selected():
+    print("Sukces, zaznaczyłem poprawnie!")
+
+# zamikniecie zakładki
+driver.close()
+
+# przełącznie sie do bieżącego okna
+driver.switch_to.window(currentWindows)
+print("Aktualne okno to: ", driver.title)
+
 time.sleep(200)
 driver.quit()

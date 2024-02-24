347a5d492ab9a21b8261fb8516a0e1e49b5539bc
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-02-24 10:00:42 +0100

    podstawowe komendy

--- cw1_uruchomienie_przegladarki.py
+++ cw1_uruchomienie_przegladarki.py
@@ -4,11 +4,32 @@ import time
 # inicjalizacja przeglądarki Chrome
 driver = webdriver.Chrome()
 
+url = "https://www.google.com"
+new_url = "https://www.wp.pl"
+
 # otwarcie okna przeglądarki z konkretnym adresem
-driver.get("https://www.google.com")
+driver.get(url)
+
+# wielkość
+# konkretna wilkość dobra dla testów
+# driver.set_window_size(400, 200)
+# maksymalizowanie okna
+driver.maximize_window()
+
+# otwarcie nowego okna
+driver.execute_script("window.open('');")
+
+# przełączenie się do nowego okna
+driver.switch_to.window(driver.window_handles[1])
+
+# uruchomienie nowej strony
+driver.get(new_url)
+
+# zamkniecie zakładki
+driver.close()
 
 # zatrzymanie skryptu na x sekund
-time.sleep(10)
+time.sleep(15)
 
 # zakończenie pracy przeglądarki
 driver.quit()

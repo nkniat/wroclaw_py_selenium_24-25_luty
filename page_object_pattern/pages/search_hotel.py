d9b6a9a928f7210a18654816a1cc8ae357bf91c2
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-02-25 09:36:10 +0100

    seach hotel page

--- page_object_pattern/pages/search_hotel.py
+++ page_object_pattern/pages/search_hotel.py
@@ -11,3 +11,22 @@ class SearchHotelPage:
         self.adults_input_name = "adults"
         self.child_input_name = "child"
         self.search_button_xpath = "//*[@id='hotels']/form/div[5]/button"
+
+    def set_city(self, city):
+        self.driver.find_element("xpath", self.search_hotel_span_xpath).click()
+        self.driver.find_element("xpath", self.search_hotel_input_xpath).send_keys(city)
+        self.driver.find_element("xpath", self.search_hotel_match_xpath).click()
+
+    def set_date_range(self, check_in, check_out):
+        self.driver.find_element("name", self.checkin_input_name).send_keys(check_in)
+        self.driver.find_element("name", self.checkout_input_name).send_keys(check_out)
+
+    def set_travellers(self, adults, kids):
+        self.driver.find_element("name", self.travellers_input_name).click()
+        self.driver.find_element("name", self.adults_input_name).clear()
+        self.driver.find_element("name", self.adults_input_name).send_keys("1")
+        self.driver.find_element("name", self.child_input_name).clear()
+        self.driver.find_element("name", self.child_input_name).send_keys("2")
+
+    def perform_search(self):
+        self.driver.find_element("xpath", self.search_button_xpath).click()

52d5a7eb7c35ab272d16f4a97f3520bbbe6c93e3
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-02-25 13:36:17 +0100

    szkielet strony + lokalizatory

---
+++ page_object_pattern_shop/locators/locators.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+
+
+class BillingAddressLocators:
+
+    reg_email_input = ("id", "reg_email")
+    reg_password_input = ("id", "reg_password")
+    register_input = ("name", "register")
+    addresses_link = (By.LINK_TEXT, "Addresses")
+    edit_link = (By.LINK_TEXT, "Edit")
+    billing_first_name_input = ("id", "billing_first_name")
+    billing_last_name_input = ("id", "billing_last_name")
+    billing_country_select = ("id", "billing_country")
+    billing_address_1_input = ("id", "billing_address_1")
+    billing_postcode_input = ("id", "billing_postcode")
+    billing_city_input = ("id", "billing_city")
+    billing_phone_input = ("id", "billing_phone")
+    save_address_button = ("name", "save_address")
+    msg_div = ("xpath", "//*[@id='page-7']/div/section/div/div/div/div[2]/div")
+
+
+class MyAccountLocators:
+    username_input = ("id", "username")
+    password_input = ("id", "password")
+    login_button = ("name", "login")
+    msg_error_div = ("xpath", "//*[@id='page-7']/div/section/div/div/div[1]/ul/li")
+    reg_email_input = ("id", "reg_email")
+    reg_password_input = ("id", "reg_password")
+    register_button = ("name", "register")
+    logout_link = (By.LINK_TEXT, "Logout")
+
